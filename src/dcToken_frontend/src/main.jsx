55a07cbd2fe8bc55e22fb6db28bf6df5422b095f
Christian <christianloumagday@gmail.com>
2025-04-10 12:09:32 +0800

    initial

--- src/dcToken_frontend/src/main.jsx
+++ src/dcToken_frontend/src/main.jsx
@@ -1,10 +1,7 @@
-import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import SignIn from './signin';
-import SignUp from './signup';
 import Create from './Create';
 import Update from './Update';
 
@@ -12,14 +9,6 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />
-  },
-  {
-    path: "/signin",
-    element: <SignIn />
-  },
-  {
-    path: "/signup",
-    element: <SignUp />
   },
   {
     path: "/create",
